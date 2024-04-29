84525e377f06e389d1415c398c889681c239adfa
MarkusSandvik <markus.sandvik@hotmail.com>
2024-04-29 11:53:47 +0200

    Worked on energyConsumption

--- energyConsumption/energyConsumption.ino
+++ energyConsumption/energyConsumption.ino
@@ -9,6 +9,9 @@ const int accelerationOffset = 0.5;
 int speedArrayCounter = 0;
 int accelerationArrayCounter = 0;
 
+float previousAverageSpeed = 0;
+float previousAverageAcceleration = 0;
+
 bool speedArrayFull = false;
 bool accelerationArrayFull = false;
 
@@ -25,7 +28,16 @@ unsigned long previousCalculationMillis = 0;
 const long calculationInterval = 10000;
 
 unsigned long previousReportMillis = 0;
-const long reportInterval = 10000;
+const long reportInterval = 20000;
+
+unsigned long previousStopRemoveMillis = 0;
+const long stopRemoveInterval = 60000;
+
+bool ignition = true;
+bool carStopped = false;
+byte startStopCounter = 0;
+
+
 
 
 void readAndStoreSpeed(){
@@ -37,17 +49,20 @@ void readAndStoreSpeed(){
         if (speedArrayCounter >= readings){
             speedArrayCounter = 0;
             speedArrayFull = true;
-        }
+        } // end if
 
         float speedReading = analogRead(speedPin);
 
-        float speed = map(speedReading, 0, 4095, 0, 40);
+        float speed = map(speedReading, 0, 4095, 0, 40);                                                                                                            // Not needed when integrated
 
-        speedArray[speedArrayCounter] = speed;
+        // Only record speed when car is driving
+        if (speed > 0){
+            speedArray[speedArrayCounter] = speed;
+        } // end if
 
         speedArrayCounter += 1;
-    } 
-}
+    } // end if
+} // end void
 
 void readAndStoreAcceleration(){
     unsigned long currentMillis = millis();
@@ -58,59 +73,105 @@ void readAndStoreAcceleration(){
         if (accelerationArrayCounter >= readings){
             accelerationArrayCounter = 0;
             accelerationArrayFull = true;
-        }
+        } // end if
 
         float accelerationReading = analogRead(accelerationPin);
-        float acceleration = map(accelerationReading, 0, 4095, 0, 50); // Ten times wanted limit since map function returns int
-        acceleration = acceleration / 10; // converts from Int value to a float
+        float acceleration = map(accelerationReading, 0, 4095, 0, 50); // Ten times wanted limit since map function returns int,                                    // Not needed when integrated
+    acceleration = acceleration / 10; // converts from Int value to a float                                                                                         // Not needed when integrated
 
+        // Only ads value if reading is not below offset --------------------------------- Issues while backing, but backing gives same readings ass deceleration
         if (acceleration >= accelerationOffset){
             accelerationArray[accelerationArrayCounter] = acceleration;
             accelerationArrayCounter += 1;
-        }
-    }
-}
+        } // end if
+    } // end if
+} // end void
 
 float getAverageSpeed(){
     float speedSum = 0;
     int speedReadings;
 
+    // Calculates for all values if array is filled on trip
     if (speedArrayFull == false){
         speedReadings = speedArrayCounter;
-    }
+    } // end if
+
+    // Calculates only for read values if array is not yet filled on trip
     else{
         speedReadings = readings;
-    }
+    } // end else
 
+    // Sums up all readings in array
     for (int i = 0; i < speedReadings; i++){
         speedSum += speedArray[i];
-    }
+    } // end for
 
-    return (speedSum / speedReadings);
-}
+    float currentAverageSpeed = (speedSum / speedReadings);
+
+    // Finds average value of current calculation and previously stored calculations.
+    float averageSpeed = (currentAverageSpeed + previousAverageSpeed) / 2; 
+    previousAverageAcceleration = averageSpeed;
+
+    // Calculates and returns the average value in this interval
+    return averageSpeed;
+} // end float
 
 float getAverageAcceleration(){
     float accelerationSum = 0;
     int accelerationReadings;
 
+    // Calculates for all values if array is filled on trip
     if (accelerationArrayFull == true){
         accelerationReadings = accelerationArrayCounter;
-    }
+    } // end if
+
+    // Calculates only for read values if array is not yet filled on trip
     else{
         accelerationReadings = readings;
-    }
+    } // end else
 
+    // Sums up all readings in array
     for (int i = 0; i < accelerationReadings; i++){
         accelerationSum += accelerationArray[i];
-    }
+    } // end for
+    
+    float currentAverageAcceleration = (accelerationSum / readings);
 
-    return (accelerationSum / readings);
+    // Finds average value of current calculation and previously stored calculations
+    float averageAcceleration = (currentAverageAcceleration + previousAverageAcceleration) / 2; 
+    previousAverageSpeed = averageAcceleration; 
+
+    // Calculates and returns the average value in this interval
+    return averageAcceleration;
+} // end float
+
+void countStartStop(){
+    unsigned long currentMillis = millis();
+    float speedReading = analogRead(speedPin);
+
+    if (speedReading == 0){
+        carStopped = true;
+    } // end if
+
+    if ((carStopped == true) && (speedReading != 0)){
+        startStopCounter += 1;
+        carStopped = false;
+    } // end if
+
+    if (currentMillis - previousStopRemoveMillis >= stopRemoveInterval){
+        previousStopRemoveMillis = currentMillis;
+        startStopCounter -= 1;
+    } // end if
+} // end void
+
+void calculateDriverScore(){
+    
 }
 
-
 void printReport(){
-    float averageSpeed = getAverageSpeed();
-    float averageAcceleration = getAverageAcceleration();
+    float averageSpeed = previousAverageSpeed;
+    float averageAcceleration = previousAverageAcceleration;  
+
     Serial.print("Average Speed");
     Serial.print("       ");
     Serial.println("Average acceleration");
@@ -132,7 +193,7 @@ void printReport(){
     }
     Serial.println("");
     Serial.println("");
-}
+} // end void
 
 
 void setup(){
@@ -140,7 +201,7 @@ void setup(){
     pinMode(speedPin, INPUT);
     pinMode(accelerationPin, INPUT);
     pinMode(startCountPin, INPUT_PULLUP);
-}
+} // end setup
 
 void loop(){
     unsigned long currentMillis = millis();
@@ -148,9 +209,8 @@ void loop(){
     readAndStoreSpeed();
     readAndStoreAcceleration();
     
-
     if (currentMillis - previousReportMillis >= reportInterval){
         previousReportMillis = currentMillis;
         printReport();
-    }
-}
+    } // end if
+} // end loop
